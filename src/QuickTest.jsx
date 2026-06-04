4c771776830d295d644f797c349a6abdd6133700
confidentha-ux <confidentha@gmail.com>
2026-06-04 14:49:11 -0700

    Update QuickTest.jsx

--- src/QuickTest.jsx
+++ src/QuickTest.jsx
@@ -285,7 +285,7 @@ export default function QuickTest({ onBack }) {
 
         {step === "intro" && (
           <div style={{textAlign:"center"}}>
-            <img src="/intro.png" alt="거울" style={{width:"300px", height:"300px", objectFit:"contain", marginBottom:"0.5rem"}}/>
+            <img src="/intro.png" alt="거울" style={{width:"400px", height:"400px", objectFit:"contain", marginBottom:"0.5rem"}}/>
             <h1 style={{fontSize:"2.8rem", fontWeight:700, color:"#222", marginBottom:"0.5rem", lineHeight:1.2}}>
               나는 어떤 사람일까?
             </h1>
